d4d6ea7e28d9334b23010f2ac94459291e4c9555
Illia <dudchik931@gmail.com>
2024-06-05 19:21:40 +0300

    fix

--- macOS/system-macos/src/app/page.tsx
+++ macOS/system-macos/src/app/page.tsx
@@ -2,12 +2,14 @@ import styles from "./window.module.css";
 import Image from 'next/image';
 import "./globals.css";
 import MenuBar from "./components/MenuBar"
+import DockPanel from "./components/DockPanel";
 export default function MainScreen() {
  
 
   return (
     <div className={styles.background}>
        <MenuBar />
+       <DockPanel/>
   </div>
   );
 }
